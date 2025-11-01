58049a9c7a8f35c00a26e9dd9e9f3ce651ac8bb1
adamwisnup <wisnuadam21@gmail.com>
2025-11-01 21:55:47 +0700

    feat: integrate Express with NestJS and update API documentation title

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
+const server = express();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   const config = new DocumentBuilder()
-    .setTitle('School API')
+    .setTitle('SMK Muhammadiyah 3 Dolopo API')
     .setDescription('API documentation SMK Muhammadiyah 3 Dolopo.')
     .setVersion('1.0')
-     .addBearerAuth()
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-  await app.listen(process.env.PORT || 3000);
+  await app.init();
 }
+
 bootstrap();
+
+export default server;
