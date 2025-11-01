58f2f7b518856832910892f9a63f84a58e9f707f
adamwisnup <wisnuadam21@gmail.com>
2025-11-01 23:00:19 +0700

    refactor: simplify main.ts by removing Express integration and updating Swagger title

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,26 @@
-import express from 'express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const server = express();
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder()
-    .setTitle('SMK Muhammadiyah 3 Dolopo API')
+    .setTitle('School API')
     .setDescription('API documentation SMK Muhammadiyah 3 Dolopo.')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document, {
-    customSiteTitle: 'SMK Muhammadiyah 3 Dolopo API Docs',
-    swaggerOptions: {
-      persistAuthorization: true,
-    },
-  });
 
-  await app.init();
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
-
 bootstrap();
-
-export default server;
